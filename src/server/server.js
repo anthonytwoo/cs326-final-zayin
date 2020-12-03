0b8bc011479b67ef89d47df6a4777311c5cb6476
shipatel <60272072+shipatel@users.noreply.github.com>
2020-12-02 19:02:15 -0500

    port

--- src/server/server.js
+++ src/server/server.js
@@ -499,4 +499,4 @@ app.get('/logout', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../', 'search.html'));
 // });
 
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
