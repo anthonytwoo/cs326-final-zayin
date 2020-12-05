99e006546bd31d085a41937757986c93f44c7e24
atmajashah <atmajashah@umass.edu>
2020-12-04 21:49:02 -0500

    fixed sign in/sign up

--- src/sign-in.js
+++ src/sign-in.js
@@ -1,25 +1,4 @@
-let signInButton = document.getElementById('signin');
 let createAccountButton = document.getElementById('createAccount');
-
-signInButton.addEventListener("click", async ()=>{
-  let username = document.getElementById("SIUsername").value;
-  let password = document.getElementById("SIPassword").value;
-  const response = await fetch('/sign-in', {
-    method: 'POST',
-    body: JSON.stringify({
-        username: username,
-        password: password
-    })
-  });
-  if (response.status === 200){
-    window.location = "/";
-    document.getElementById('passwordLabel').innerHTML=""
-    document.getElementById("signinHalf").style.visibility = "hidden";
-  }
-  else{
-    document.getElementById('passwordLabel').innerHTML="Incorrect username or password."
-  }
-});
 
 createAccountButton.addEventListener("click", async ()=>{
     let username = document.getElementById("SUUsername").value;
