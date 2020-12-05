6d623304a40627f0eb07d8c6645a3ae71733c77d
shipatel <60272072+shipatel@users.noreply.github.com>
2020-12-04 20:52:13 -0500

    sign in/up password update

--- src/server/server.js
+++ src/server/server.js
@@ -525,4 +525,4 @@ app.get('/logout', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../', 'search.html'));
 // });
 
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8080);
